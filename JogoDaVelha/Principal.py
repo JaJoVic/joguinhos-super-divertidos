13c7e7aba307a7e7bcd1cd0f7d559b9bdd750732
João Pedro <fonsecajope@gmail.com>
2020-12-07 12:43:34 -0300

    Cria Principal.py e concluí funcionamento gráfico

--- JogoDaVelha/Principal.py
+++ JogoDaVelha/Principal.py
@@ -2,7 +2,7 @@ import turtle
 
 Tela = turtle.Screen()
 Tela.setup(600,600)
-Tela.setworldcoordinates(0,0,3,3)
+Tela.setworldcoordinates(0,0,5,5)
 Tela.tracer(0,0)
 turtle.hideturtle()
 
@@ -11,15 +11,15 @@ JogaO = False
 
 def DesenhaTabuleiro():
     turtle.pensize(10)    
-    for i in {1,2}:        
+    for i in {2,3}:        
         turtle.up()
-        turtle.goto(0,i)
+        turtle.goto(1,i)
         turtle.seth(0)
         turtle.down()
         turtle.fd(3)
     
         turtle.up()
-        turtle.goto(i,0)
+        turtle.goto(i,1)
         turtle.seth(90)
         turtle.down()
         turtle.fd(3)
@@ -65,39 +65,10 @@ def RegistraJogada(x,y):
         JogaO = False
         JogaX = True
 
-def ConfereResultado():    
-    Xganhou = False
-    Oganhou = False
-
-    for i in {0,1,2}:
-        if Casa[i][0] == Casa[i][1] and Casa[i][1] == Casa[i][2] and Casa[i][2] == "X":
-            Tela.textinput("Game over! ","X ganhou!")
-            Xganhou = True
-        elif Casa[i][0] == Casa[i][1] and Casa[i][1] == Casa[i][2] and Casa[i][2] == "X":
-            Tela.textinput("Game over! ","O ganhou!")
-            Oganhou = True
-
-        break
-
-        for j in {0,1,2}:
-            if Casa[0][j] == Casa[1][j] and Casa[1][j] == Casa[2][j] and Casa[2][j] == "X":
-                Tela.textinput("Game over! ","X ganhou!")
-                Xganhou = True
-            elif Casa[0][j] == Casa[1][j] and Casa[1][j] == Casa[2][j] and Casa[2][j] == "X":
-                Tela.textinput("Game over! ","O ganhou!")
-                Oganhou = True
-    
-    if Xganhou and Oganhou:
-        Tela.textinput("Game over! ","Empate!")
-
-def Jogo():
-    DesenhaTabuleiro()
-    Tela.onclick(RegistraJogada)
-    ConfereResultado()
+DesenhaTabuleiro()
+Tela.onclick(RegistraJogada)
 
 Casa = [[0,0,0],[0,0,0],[0,0,0]]
-
-Jogo()
 
 turtle.mainloop()
 
